d174a28f386306e9f7a12d1f1c53749895395c72
Nurul Amin Shakil <41535333+Shakil1995@users.noreply.github.com>
2022-06-02 18:52:27 +0600

    template mastering

---
+++ resources/views/layout/partials/script.blade.php
@@ -0,0 +1,4 @@
+<script src="{{ asset('assets/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{asset('assets/js/chart.min.js') }}"></script>
+<script src="{{ asset('assets/js/bs-init.js') }}"></script>
+<script src="{{ asset('assets/js/theme.js') }}"></script>
